21035374cd213cf271bf4640952ae95582d73471
SimonForshaw <Aten_Shun@hotmail.co.uk>
2024-06-24 17:34:14 +0100

    Arrays, bestPractices, count, flowControl and part of functions completed

--- app/count.js
+++ app/count.js
@@ -1,7 +1,27 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let count = start;
+    let intervalId;
 
-  }
+    function startCounting() {
+      console.log(count);
+      count++;
+      if (count > end) {
+        clearInterval(intervalId);
+      }
+    }
+
+    intervalId = setInterval(startCounting, 100);
+
+    console.log(count);
+    count++;
+
+    return {
+      cancel: function () {
+        clearInterval(intervalId);
+      },
+    };
+  },
 };
