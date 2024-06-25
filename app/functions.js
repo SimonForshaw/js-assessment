6d3acd93746123e3e9e8c0f83e21b2403ea7ae4b
SimonForshaw <Aten_Shun@hotmail.co.uk>
2024-06-25 10:31:20 +0100

    over 50% passing tests

--- app/functions.js
+++ app/functions.js
@@ -32,13 +32,54 @@ exports.functionsAnswers = {
     return result;
   },
 
-  partial: function (fn, str1, str2) {},
+  //you should be able to create a "partial" function
+  partial: function (fn, str1, str2) {
+    return function (str3) {
+      return fn(str1, str2, str3);
+    };
+  },
 
-  useArguments: function () {},
+  //you should be able to use arguments
+  useArguments: function () {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+    return sum;
+  },
 
-  callIt: function (fn) {},
+  //you should be able to apply functions with arbitrary numbers of arguments
+  callIt: function (fn) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    return fn.apply(null, args);
+  },
 
-  partialUsingArguments: function (fn) {},
+  //you should be able to create a "partial" function for variable number of applied arguments
+  partialUsingArguments: function (fn) {
+    let initialArgs = Array.prototype.slice.call(arguments, 1);
 
-  curryIt: function (fn) {},
+    return function () {
+      let newArgs = Array.prototype.slice.call(arguments);
+      let allArgs = initialArgs.concat(newArgs);
+      return fn.apply(null, allArgs);
+    };
+  },
+
+  //you should be able to curry existing functions
+  curryIt: function (fn) {
+    let arity = fn.length;
+
+    function curryHelper(fn, args) {
+      return function (arg) {
+        let newArgs = args.concat(arg);
+        if (newArgs.length >= arity) {
+          return fn.apply(null, newArgs);
+        } else {
+          return curryHelper(fn, newArgs);
+        }
+      };
+    }
+
+    return curryHelper(fn, []);
+  },
 };
